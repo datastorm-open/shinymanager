68ae7239e570436e3eea2a1fed7a3ea444e49d7c
pvictor <perrier.victor@gmail.com>
2019-04-08 11:48:08 +0200

    starting using tests

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(shinymanager)
+
+test_check("shinymanager")
---
+++ tests/testthat/test-check-credentials.R
@@ -0,0 +1,33 @@
+context("test-check-credentials")
+
+test_that("check_credentials works", {
+
+  credentials <- data.frame(
+    user = c("fanny", "victor"),
+    password = c("azerty", "12345"),
+    stringsAsFactors = FALSE
+  )
+
+  expect_is(check_credentials(credentials), "function")
+  expect_is(check_credentials(credentials)("fanny", "azerty"), "list")
+  expect_true(check_credentials(credentials)("fanny", "azerty")$result)
+  expect_false(check_credentials(credentials)("fanny", "12345")$result)
+})
+
+
+test_that("check_credentials (expired) works", {
+
+  credentials <- data.frame(
+    user = c("fanny", "victor", "benoit"),
+    password = c("azerty", "12345", "azerty"),
+    comment = c("alsace", "auvergne", "bretagne"),
+    expire = Sys.Date() + c(10, -10, 10),
+    stringsAsFactors = FALSE
+  )
+
+  expect_true(check_credentials(credentials)("fanny", "azerty")$result)
+  expect_false(check_credentials(credentials)("fanny", "azerty")$expired)
+  expect_false(check_credentials(credentials)("victor", "12345")$result)
+  expect_true(check_credentials(credentials)("victor", "12345")$expired)
+})
+
