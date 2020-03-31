a9adfe7933e47af0c2832fe8a126a50a5889db07
pvictor <perrier.victor@gmail.com>
2020-03-31 17:22:50 +0200

    secure_server() : added keep_token arg

--- R/secure-app.R
+++ R/secure-app.R
@@ -30,7 +30,7 @@ secure_app <- function(ui, ..., enable_admin = FALSE, head_auth = NULL, theme = 
     warning("Only supported language for the now are: en, fr, pt-BR", call. = FALSE)
     language <- "en"
   }
-  
+
   lan <- use_language(language)
   ui <- force(ui)
   enable_admin <- force(enable_admin)
@@ -38,7 +38,7 @@ secure_app <- function(ui, ..., enable_admin = FALSE, head_auth = NULL, theme = 
   if (is.null(theme)) {
     theme <- "shinymanager/css/readable.min.css"
   }
-  
+
   function(request) {
     query <- parseQueryString(request$QUERY_STRING)
     token <- query$token
@@ -81,7 +81,7 @@ secure_app <- function(ui, ..., enable_admin = FALSE, head_auth = NULL, theme = 
                 tooltip = lan$get("Go to application"),
                 icon = icon("share")
               )
-            ), 
+            ),
             shinymanager_where("admin")
           ),
           tabPanel(
@@ -164,28 +164,30 @@ secure_app <- function(ui, ..., enable_admin = FALSE, head_auth = NULL, theme = 
 #' @param check_credentials Function passed to \code{\link{auth_server}}.
 #' @param timeout Timeout session (minutes) before logout if sleeping. Defaut to 15. 0 to disable.
 #' @param inputs_list \code{list}. If database credentials, you can configure inputs for editing users information. See Details.
-#' @param fileEncoding 	character string: Encoding of logs downloaded file. See \code{\link{write.table}}
+#' @param fileEncoding 	character string: Encoding of logs downloaded file. See \code{\link{write.table}}.
+#' @param keep_token Logical, keep the token used to authenticate in the URL, it allow to refresh the
+#'  application in the browser, but careful the token can be shared between users ! Default to \code{FALSE}.
 #' @param session Shiny session.
-#' 
-#' @details 
-#' 
-#' If database credentials, you can configure inputs with \code{inputs_list} for editing users information 
-#' from the admin console. \code{start}, \code{expire}, \code{admin} and \code{password} are not configurable. 
+#'
+#' @details
+#'
+#' If database credentials, you can configure inputs with \code{inputs_list} for editing users information
+#' from the admin console. \code{start}, \code{expire}, \code{admin} and \code{password} are not configurable.
 #' The others columns are rendering by defaut using a \code{textInput}. You can modify this using \code{inputs_list}.
 #' \code{inputs_list} must be a named list. Each name must be a column name, and then we must have the function
-#'  shiny to call \code{fun} and the arguments \code{args} like this : 
+#'  shiny to call \code{fun} and the arguments \code{args} like this :
 #'  \code{
 #'  list(group = list(
-#'      fun = "selectInput", 
+#'      fun = "selectInput",
 #'      args = list(
-#'          choices = c("all", "restricted"), 
-#'          multiple = TRUE, 
+#'          choices = c("all", "restricted"),
+#'          multiple = TRUE,
 #'          selected = c("all", "restricted")
 #'       )
 #'      )
 #' )
 #' }
-#' 
+#'
 #' @export
 #'
 #' @importFrom shiny callModule getQueryString parseQueryString
@@ -193,12 +195,15 @@ secure_app <- function(ui, ..., enable_admin = FALSE, head_auth = NULL, theme = 
 #'
 #' @rdname secure-app
 secure_server <- function(check_credentials, timeout = 15, inputs_list = NULL,
-                          fileEncoding = "",
+                          fileEncoding = "", keep_token = FALSE,
                           session = shiny::getDefaultReactiveDomain()) {
-  
-  isolate(resetQueryString(session = session))
   token_start <- isolate(getToken(session = session))
-  
+  if (isTRUE(keep_token)) {
+    .tok$reset_count(token_start)
+  } else {
+    isolate(resetQueryString(session = session))
+  }
+
   lan <- reactiveVal(use_language())
   observe({
     lang <- getLanguage(session = session)
@@ -206,48 +211,48 @@ secure_server <- function(check_credentials, timeout = 15, inputs_list = NULL,
       lan(use_language(lang))
     }
   })
-  
+
   callModule(
     module = auth_server,
     id = "auth",
     check_credentials = check_credentials,
-    use_token = TRUE, 
+    use_token = TRUE,
     lan = lan
   )
-  
+
   callModule(
     module = pwd_server,
     id = "password",
     user = reactiveValues(user = .tok$get(token_start)$user),
     update_pwd = update_pwd,
-    use_token = TRUE, 
+    use_token = TRUE,
     lan = lan
   )
-  
+
   .tok$set_timeout(timeout)
-  
+
   path_sqlite <- .tok$get_sqlite_path()
   if (!is.null(path_sqlite)) {
     callModule(
       module = admin,
       id = "admin",
       sqlite_path = path_sqlite,
-      passphrase = .tok$get_passphrase(), 
-      inputs_list = inputs_list, 
+      passphrase = .tok$get_passphrase(),
+      inputs_list = inputs_list,
       lan = lan
     )
     callModule(
       module = logs,
       id = "logs",
       sqlite_path = path_sqlite,
-      passphrase = .tok$get_passphrase(), 
-      fileEncoding = fileEncoding, 
+      passphrase = .tok$get_passphrase(),
+      fileEncoding = fileEncoding,
       lan = lan
     )
   }
-  
+
   user_info_rv <- reactiveValues()
-  
+
   observe({
     token <- getToken(session = session)
     if (!is.null(token)) {
@@ -267,21 +272,21 @@ secure_server <- function(check_credentials, timeout = 15, inputs_list = NULL,
       }
     }
   })
-  
+
   observeEvent(session$input$.shinymanager_admin, {
     token <- getToken(session = session)
     updateQueryString(queryString = sprintf("?token=%s&admin=true&language=%s", token, lan()$get_language()), session = session, mode = "replace")
     .tok$reset_count(token)
     session$reload()
   }, ignoreInit = TRUE)
-  
+
   observeEvent(session$input$.shinymanager_app, {
     token <- getToken(session = session)
     updateQueryString(queryString = sprintf("?token=%s&language=%s", token, lan()$get_language()), session = session, mode = "replace")
     .tok$reset_count(token)
     session$reload()
   }, ignoreInit = TRUE)
-  
+
   observeEvent(session$input$.shinymanager_logout, {
     token <- getToken(session = session)
     logout_logs(token)
@@ -289,11 +294,11 @@ secure_server <- function(check_credentials, timeout = 15, inputs_list = NULL,
     clearQueryString(session = session)
     session$reload()
   }, ignoreInit = TRUE)
-  
-  
-  
+
+
+
   if (timeout > 0) {
-    
+
     observeEvent(session$input$.shinymanager_timeout, {
       token <- getToken(session = session)
       if (!is.null(token)) {
@@ -305,7 +310,7 @@ secure_server <- function(check_credentials, timeout = 15, inputs_list = NULL,
         }
       }
     })
-    
+
     observe({
       invalidateLater(30000, session)
       token <- getToken(session = session)
@@ -318,9 +323,9 @@ secure_server <- function(check_credentials, timeout = 15, inputs_list = NULL,
         }
       }
     })
-    
+
   }
-  
+
   return(user_info_rv)
 }
 
